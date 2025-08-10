ee76726833533f0cd87411ddba479f1c9b794ffe
AugustoAlmondes <augusto7666@gmail.com>
2025-08-10 00:49:55 -0300

    refactor: Added comic rarity in hook useComics

--- src/hooks/useComics.tsx
+++ src/hooks/useComics.tsx
@@ -3,9 +3,15 @@ import { getComics } from '../api/marvel';
 import type { ComicCardType } from '../types/comic';
 
 export function useComics() {
-    return useQuery<ComicCardType[]|Error>({
+    return useQuery<ComicCardType[]>({
         queryKey: ['comics'],
         queryFn: () => getComics().then(res => res.data.data.results),
-        staleTime: Infinity
+        staleTime: Infinity,
+        select: (data) => {
+            return data.map(comic => ({
+                ...comic,
+                isRare: Math.random() < 0.1
+            }))
+        }
     });
 }
